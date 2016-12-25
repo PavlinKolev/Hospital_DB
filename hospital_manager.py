4ee4a444b3fb2454367566f40add1d764706a2e8
PavlinKolev <pavkata993@gmail.com>
2016-12-25 03:07:50 +0200

    Hospital Manager class

---
+++ hospital_manager.py
@@ -0,0 +1,73 @@
+import os
+import sys
+from hospital_data_base import HospitalDB
+from doctor import Doctor
+from patient import Patient
+from populate_starting_data import populate_hospital, is_hospital_existing
+from settings import COMMANDS_COUNT
+from password import encode
+from validations import validate_password
+from helper_prints import(
+                        print_help_menu,
+                        print_choose_injuries,
+                        print_choose_academic_title)
+
+
+class HospitalManager:
+    def __init__(self, hospital_name):
+        hospital_name = hospital_name.replace(' ', '-')
+        db_was_not_existing = not(is_hospital_existing(hospital_name))
+        self.hospital = HospitalDB(hospital_name)
+        self.commands = self.__make_function_dict()
+        # only if there was no database with this name, we populate it with data
+        if db_was_not_existing:
+            populate_hospital(self.hospital)
+
+    def run_interface(self):
+        while True:
+            os.system('clear')
+            self.__start_menu()
+            try:
+                command = int(input("command:> "))
+                self.__execute_command(command)
+            except ValueError:
+                input("Wrong input: No such command.\n" +
+                        "Press Enter to continue...")
+
+    def __execute_command(self, command):
+        try:
+            if command in range(1, COMMANDS_COUNT + 1):
+                self.commands[command]()
+            else:
+                print("Wrong input: No such command.")
+        except ValueError as err:
+            print("Wrong input: ", err)
+        input("Press Enter to continue...")
+
+    def __login(self):
+        username = input("username:> ")
+        password = input("password:> ")
+        self.hospital.login_user(username, password)
+
+    def __register(self):
+        username = input("username:> ")
+        password = input("password:> ")
+        validate_password(password)
+        pass_2 = input("password again:> ")
+        if password != pass_2:
+            raise ValueError("Different password.")
+        age = int(input("age:> "))
+        self.hospital.register_user(username, password, age)
+
+    def __make_function_dict(self):
+        return {
+                1: self.__login,
+                2: self.__register,
+                3: self.__start_menu,
+                4: self.__exit}
+
+    def __start_menu(self):
+        print_help_menu()
+
+    def __exit(self):
+        sys.exit()
---
+++ settings.py
@@ -0,0 +1,58 @@
+DEFAULT_HOSPITAL_NAME = 'hospital'
+
+MIN_PASS_LENGTH = 8
+
+ACADEMIC_TITLES = [
+                'Allergist',
+                'Anesthesiologist',
+                'Cardiologist',
+                'Dentist',
+                'Dermatologist',
+                'Gynecologist',
+                'Oncologist',
+                'Pediatrician',
+                'Urologist']
+
+INJURIES = [
+            'Cancer',
+            'Diabetes',
+            'Eczema',
+            'Epilepsy',
+            'Hepatitis',
+            'Laryngitis',
+            'Leukemia',
+            'Lupus']
+
+COMMANDS_COUNT = 4
+
+COMMANDS = {
+            1: "Log into the system",
+            2: "Register as a new user",
+            3: "Help main",
+            4: "Exit"}
+
+DOCTOR_COMMANDS_COUNT = 8
+
+DOCTOR_COMMANDS = {
+        0: "List Options",
+        1: "List all of your patients",
+        2: "Add hours for visitation",
+        3: "Delete free hours of visitation",
+        4: "See the room and the duration of hospital stays per each of your patients",
+        5: "Change your username",
+        6: "Change your age",
+        7: "Log out"}
+
+PATIENT_COMMANDS_COUNT = 10
+
+PATIENT_COMMANDS = {
+        0: "List Options",
+        1: "See the free hours of your doctor",
+        2: "Reserve hour for visitation",
+        3: "Stay at the hospital for an injury",
+        4: "See the academic title of his doctor",
+        5: "List your hospital stays",
+        6: "Change your doctor",
+        7: "Change your username",
+        8: "Change your age",
+        9: "Log out"}
